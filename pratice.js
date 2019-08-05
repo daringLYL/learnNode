96c6ccc9caca31024e953ae5c63d40597174b34a
daringLYL <1032982127@qq.com>
2019-08-05 21:15:07 +0800

    往端口上传一个数组对象

--- pratice.js
+++ pratice.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
-app.get('/',function(req,res){
-    res.send('Helloworld')
+app.get('/toDo', function (req, res) {
+    res.send(
+        {code:200,
+            result: [{ "name": "例如", "date": "2019/7/28" }, { "name": "啦啦啦", "date": "2019/7/28" },
+            { "name": "待办事项不见了", "date": "2019/7/25" }, { "name": "好好", "date": "2019/7/25" },
+            { "name": "不想吵架", "date": "2019/7/25" }
+            ]
+        }
+    )
 })
-app.listen(8080,()=>{
+app.listen(8080, () => {
     console.log('App listening on port 8080!');
 });
